5d8887069ccdb2d9f89a7026a3e0b9f98fe9bbc8
Himanshumishra2161 <86967357+Himanshumishra2161@users.noreply.github.com>
2022-07-27 22:27:59 +0530

    Add files via upload

---
+++ hp.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>IMS IFFCO Homepage - Inventory Management System</title>
+    <link rel="stylesheet" type="text/css" href="css1/hp.css">
+    <script src="http://use.fontawesome.com/0c7a3095b5.js"></script>
+</head>
+<body>
+    <div class="header">
+        <div class="homepageContainer">
+            <a href="login.php">LogIn</a>
+        </div>
+        
+    </div>
+    <div class="banner">
+        <div class="homepageContainer">
+            <div class="bannerHeader">
+                <h1>IMS IFFCO</h1>
+                <p>Inventory Management System</p>
+            </div>
+            <p class="bannerTagline">
+                Track your goods throughout your entire supply chain, from purchasing to production to end sales
+            </p>
+            <div class="bannerIcons">
+                <a href=""><i class="fa fa-apple" aria-hidden="true"></i></a>
+                <a href=""><i class="fa fa-windows" aria-hidden="true"></i></a>
+                <a href=""><i class="fa fa-android" aria-hidden="true"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="homepageContainer">
+        <div class="homepageFeatures">
+            <div class="homepageFeature">
+                <span class="featureIcon"><i class="fa fa-info"></i></span>
+                <h3 class="featureTitle" style="font-weight: bold;">About</h3>
+                <p class="featureDescription">Indian Farmers Fertiliser Cooperative Limited, also known as IFFCO, is a multi-state cooperative society. IFFCO is wholly owned by Cooperative Societies of India. The society is engaged in the business of manufacturing and marketing of fertilizers. IFFCO is headquartered in New Delhi, India.</p>
+            </div>
+            <div class="homepageFeature">
+                <span class="featureIcon"><i class="fa fa-tasks"></i></span>
+                <h3 class="featureTitle" style="font-weight: bold;">Mission of IFFCO</h3>
+                <p class="featureDescription">Like every major cooperative out there, IFFCO also has certain aims and goals that it wants to achieve and help make the world a better place for others. Here’s a list of ways in which IFFCO aims to contribute to the country’s growth and prosperity:
+
+To provide quality fertilizers to farmers across the country in a fast and timely manner
+
+To product and manufacture fertilizers in an eco-friendly manner to conserve energy and
+resources
+
+To gather and make use of advanced cost-effective and eco-friendly technologies for faster progress</p>
+
+            </div>
+            <div class="homepageFeature">
+                <span class="featureIcon"><i class="fa fa-globe"></i></span>
+                <h3 class="featureTitle" style="font-weight: bold;">Associates and subsidiaries</h3>
+                <p class="featureDescription">IFFCO Tokio General Insurance Company Limited | 
+                    IFFCO eBazar Limited | 
+                    IFFCO Kisan Sanchar Limited | 
+                    Oman India Fertiliser Company SAOC | 
+                    National Commodity & Derivatives Exchange Ltd. | 
+                    Kisan International Trading FZE | 
+                    Indian Potash Limited | 
+                    Jordan India Fertilizer Company LLC | 
+                    IFFCO MC Crop Sciences Pvt.Ltd. | 
+                    Industries Chemiques Du Senegal</p>
+                    
+            </div>
+        </div>
+    </div>
+    <div class="homepageNotified">
+        <div class="homepageContainer">
+            <div class="homepageNotifiedContainer">
+                <div class="emailform">
+                    <h3 style="font-weight: bold; font-family: arial; font-size: xx-large;"><img src="Images/iffco_logo.png" /></h3>
+                    <h3 style="font-weight: bold;">Get Notified of any Updates!</h3>
+                    <form action="">
+                       <div class="formContainer">
+                        <input type="text" placeholder="Email Address" />
+                            <button>Notify</button>
+                       </div>
+                    </form>
+                </div>
+                <div class="video">
+                    <iframe src="https://www.youtube.com/embed/jv_dxzVp7a0" width="300px" height="200px" frameborder="0"></iframe>
+                </div>
+            </div>
+        
+        </div> 
+    </div>
+    <div class="socials">
+        <div class="homepageContainer">
+            <h3 class="socialHeader" style="font-weight: bold;">Say Hi & Get in Touch with IFFCO</h3>
+            <p class="socialText">Copyright &copy; Inventory Management System 2022</p>
+            <div class="socialIconsContainer">
+                <a href=""><i class="fa fa-twitter"></i></a>
+                <a href=""><i class="fa fa-facebook"></i></a>
+                <a href=""><i class="fa fa-instagram"></i></a>
+                <a href=""><i class="fa fa-google"></i></a>
+                <a href=""><i class="fa fa-linkedin"></i></a>
+                <a href=""><i class="fa fa-youtube"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="footer">
+        <div class="homepageContainer">
+            <a href="">Contact</a>
+            <a href="">Email</a>
+            <a href="">Support</a>
+            <a href="">Privacy Policy</a>
+        </div>
+    </div>
+</body>
+</html>
+        
